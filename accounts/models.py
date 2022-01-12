e52b1c9fa217fd810fecbfaad1c9b03f0521c6ef
yhw0536 <yhw0536@gmail.com>
2022-01-12 12:00:37 +0900

    커스텀 유저 모델 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = "M", "남성"
+        FEMALE = "F", "여성"
+
+    gender = models.CharField('성별', max_length=1, blank=True, choices=GenderChoices.choices)
+    avatar = models.ImageField('아바타', blank=True, upload_to="accounts/avatar/%Y/%m/%d",
+                               help_text="100px * 100px 크기의 gif/png/jpg 이미지를 업로드해주세요.")
