654b1a318d9e40d8595a481f6a3c3dbcf97aec2d
yhw0536 <yhw0536@gmail.com>
2022-01-13 13:03:29 +0900

    migrate시 자동으로 유저 생성

--- accounts/migrations/0001_initial.py
+++ accounts/migrations/0001_initial.py
@@ -5,6 +5,22 @@ import django.contrib.auth.validators
 from django.db import migrations, models
 import django.utils.timezone
 
+from accounts.models import User
+
+
+def gen_master(apps, schema_editor):
+    User.objects.create_user(username="admin", password="admin", first_name="관리자", email="", gender="F",
+                             is_superuser=True, is_staff=True)
+    for id in range(1, 4):
+        username = f"user{id}"
+        password = f"user{id}"
+        first_name = f"이름{id}"
+        last_name = f"성{id}"
+        email = f"test{id}@test.com"
+        gender = 'M'
+
+        User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name, email=email, gender=gender)
+
 
 class Migration(migrations.Migration):
 
@@ -43,4 +59,5 @@ class Migration(migrations.Migration):
                 ('objects', django.contrib.auth.models.UserManager()),
             ],
         ),
+        migrations.RunPython(gen_master),
     ]
