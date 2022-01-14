89effce7f6647c2a5f012b80899829bb84236d89
yhw0536 <yhw0536@gmail.com>
2022-01-14 12:34:32 +0900

    페이징 버그 수정

--- articles/views.py
+++ articles/views.py
@@ -5,6 +5,7 @@ from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.utils import timezone
 
+import articles
 from articles.forms import ArticlesForm
 from articles.models import Article
 
@@ -15,12 +16,12 @@ def articles_list(request: HttpRequest):
     search_keyword = request.GET.get('search_keyword', '')
 
     if not search_keyword:
-        products = Article.objects.order_by('-id')
+        articles = Article.objects.order_by('-id')
     else:
-        products = Article.objects.filter(title__icontains=search_keyword).order_by('-id')
+        articles = Article.objects.filter(title__icontains=search_keyword).order_by('-id')
 
     page = int(request.GET.get('page', 1))
-    paginator = Paginator(products, 4)
+    paginator = Paginator(articles, 4)
     a_list = paginator.get_page(page)
 
     return render(request, "articles/articles_list.html", {
