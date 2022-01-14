ddda60201e1133c90a8a4a24ee879635ed439a98
yhw0536 <yhw0536@gmail.com>
2022-01-14 13:11:40 +0900

    포스트 작성 폼 수정

--- articles/forms.py
+++ articles/forms.py
@@ -6,4 +6,9 @@ from articles.models import Article
 class ArticlesForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ['title', 'content']
+        fields = ['title', 'tag', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+        }
