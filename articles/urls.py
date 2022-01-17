73732b28cae0f12932925f74fa07050287ae14d1
yhw0536 <yhw0536@gmail.com>
2022-01-17 12:57:41 +0900

    포스트 수정기능 구현

--- articles/urls.py
+++ articles/urls.py
@@ -7,5 +7,6 @@ app_name = 'articles'
 urlpatterns = [
     path('', views.articles_list, name='list'),
     path('articles/<int:articles_id>/', views.articles_detail, name='detail'),
+    path('articles/modify/<int:articles_id>/', views.articles_modify, name='modify'),
     path('create/', views.articles_create, name='create'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -54,3 +54,23 @@ def articles_create(request):
         form = ArticlesForm()
         return render(request, "articles/articles_create.html",
                       {'form': form})
+
+
+def articles_modify(request, articles_id):
+    """질문수정"""
+    articles = get_object_or_404(Article, pk=articles_id)
+    if request.user != articles.user:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('articles:detail', articles_id=articles.id)
+
+    if request.method == "POST":
+        form = ArticlesForm(request.POST, instance=articles)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()  # 수정일시 저장
+            question.save()
+            return redirect('articles:detail', articles_id=articles.id)
+    else:
+        form = ArticlesForm(instance=articles)
+    context = {'form': form}
+    return render(request, 'articles/articles_create.html', context)
